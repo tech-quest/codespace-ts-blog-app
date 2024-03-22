9591e5850b7856ea0623ed7f0ea549b3e32e7723
kuro-xx <k.kuroda@miwaza.co.jp>
2024-03-22 17:49:16 +0900

    first commit

---
+++ src/app/(unique)/admin/hooks/index.ts
@@ -0,0 +1,14 @@
+import { useDeleteArticle } from './use-delete-article';
+import { useFetchArticles } from './use-fetch-articles';
+
+export const useHooks = () => {
+  const { articles, fetchError, fetchStudyError, isLoading, refetch } = useFetchArticles();
+  const { deleteError, deleteStudyError, isDeleting, deleteArticle } = useDeleteArticle();
+
+  const handleDelete = async (id: string) => {
+    await deleteArticle(undefined, { url: `/api/admin/articles/${id}` });
+    await refetch();
+  };
+
+  return { articles, fetchError, fetchStudyError, isLoading, deleteError, deleteStudyError, isDeleting, handleDelete };
+};
